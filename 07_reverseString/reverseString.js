b8be75b8b4cd43946ee5c2abcbd043733e896f86
AmirGhandiZ <amirghandi86@gmail.com>
2025-08-13 12:14:33 +0330

    Add function to reverse a string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  // split the string into something else and then JOIN it back into a string.
+  // use split to make string into chars and an array
+  // then use sort to sort the array items (chars) decending (backwards)
+  // then join/concat them back together
 
+  let test = string.split("").reverse().join("");
+  return test;
 };
 
+console.log(reverseString("hello there"));
 // Do not edit below this line
 module.exports = reverseString;
