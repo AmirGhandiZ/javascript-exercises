189a54d09cc6f011e528666179977a0876a44629
AmirGhandiZ <amirghandi86@gmail.com>
2025-08-13 14:03:53 +0330

    Add function to remove elements from an array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  console.log(args);
+
+  let filteredArray = array.filter((item) => !args.includes(item));
+  return filteredArray;
 };
+
+console.log(removeFromArray([1, 2, 3, 4, 5], 1, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
