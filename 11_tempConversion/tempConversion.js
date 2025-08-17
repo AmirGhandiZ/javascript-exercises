ea8e103749a640771669c9e7d0aa3f5c21459038
AmirGhandiZ <amirghandi86@gmail.com>
2025-08-17 10:11:26 +0330

    Add function to convert temperatures

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  // (°C) = (Temperature in degrees Fahrenheit (°F) - 32) * 5/9
+
+  let tempInCelsius = ((temp - 32) * 5) / 9;
+  let tempRounded = Math.round(tempInCelsius * 10) / 10;
+  return tempRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  // (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32
+
+  let tempInFahrenheit = (temp * 9) / 5 + 32;
+  let tempRounded = Math.round(tempInFahrenheit * 10) / 10;
+  return tempRounded;
 };
+
+console.log(convertToCelsius(32));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
