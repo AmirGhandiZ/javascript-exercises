c9306e8b4b98865fdff11ce7748a9b8f990a6823
AmirGhandiZ <amirghandi86@gmail.com>
2025-09-21 21:03:22 +0330

    Competed exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let cleanStr = str.replace(/[^a-zA-Z^1-9|s]/g, "").toLowerCase();
+  let reversed = cleanStr.split("").reverse().join("");
+  if (reversed === cleanStr) return true;
+  else return false;
 };
 
 // Do not edit below this line
